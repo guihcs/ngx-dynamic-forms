d5e9d99b2aa75c8841c9d41a01a56e172946cbea
guilherme <guilherme.fafic@gmail.com>
2020-06-02 01:22:53 -0300

    Project Start
    Signed-off-by: guilherme <guilherme.fafic@gmail.com>

---
+++ projects/dynamic-forms/src/lib/utils/utils.ts
@@ -0,0 +1,29 @@
+export function toPascalCase(text: string) {
+  return text[0].toUpperCase() + text.substr(1).toLowerCase();
+}
+
+
+export function assign(target, src, depth) {
+  if (depth <= 0) {
+    return;
+  }
+  for (const key of Object.keys(src)) {
+
+    if (src[key] && typeof src[key] === 'object') {
+      if (target[key]) {
+        assign(target[key], src[key], depth - 1);
+      }else {
+        target[key] = src[key];
+      }
+
+    } else {
+      try {
+        target[key] = src[key];
+      }catch (e) {
+
+      }
+
+    }
+  }
+}
+
