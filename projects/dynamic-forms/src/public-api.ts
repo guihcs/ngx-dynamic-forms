b0246ae01cdce2c117e97f33d08bad2c7d2ceb40
guilherme <guilherme.fafic@gmail.com>
2020-09-04 11:38:53 -0300

    Revert "Create form-rendering.md"
    This reverts commit 57621adef4fef3025df7870d097a15c663e02394.

--- projects/dynamic-forms/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dynamic-forms
- */
-
-export * from './lib/dynamic-forms.service';
-export * from './lib/dynamic-forms.component';
-export * from './lib/dynamic-forms.module';
